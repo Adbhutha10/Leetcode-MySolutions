e741d171a283862f721c68732352c49a562c96d7
Adbhutha Beere <23211a0533@bvrit.ac.in>
2026-07-12 11:33:02 +0530

    Time: 8 ms (91.94%) | Memory: 49.2 MB (42.66%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,13 +2,15 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         List<int[]> ans = new ArrayList<>();
-        ans.add(intervals[0]);
-        for (int i = 1; i < intervals.length; ++i) {
-            int s = intervals[i][0], e = intervals[i][1];
-            if (ans.get(ans.size() - 1)[1] < s) {
-                ans.add(intervals[i]);
-            } else {
-                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], e);
+        int[] curr=intervals[0];
+        ans.add(curr);
+        for (int[] i:intervals) {
+            if(i[0]<=curr[1]) {
+                curr[1]=Math.max(curr[1], i[1]);
+            }
+            else {
+                curr = i;
+                ans.add(curr);
             }
         }
         return ans.toArray(new int[ans.size()][]);
