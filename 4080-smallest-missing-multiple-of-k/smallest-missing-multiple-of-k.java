b7cff9f1e12437087e2bb3d63d97b1bdc8ea432e
Adbhutha Beere <23211a0533@bvrit.ac.in>
2026-08-26 00:08:11 +0530

    Time: 2 ms (68.20%) | Memory: 45.6 MB (15.51%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums) set.add(i);
+        int cur=k;
+        while(set.contains(cur)) cur+=k;
+        return cur;
+    }
+}
